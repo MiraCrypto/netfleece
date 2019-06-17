ca347b02f0663dfdb294c6372848784be842d4b6
nago <nago@malie.io>
2019-06-16 21:43:42 -0400

    module; refactor and export common targets

--- netfleece/__init__.py
+++ netfleece/__init__.py
@@ -1,4 +1,6 @@
-from . import netfleece
-from . import b64stream
+from .netfleece import DNBinary
+from .netfleece import main
+from .netfleece import parseloop
+from .b64stream import Base64Stream
 
 name = "netfleece"
